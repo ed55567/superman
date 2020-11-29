9f07896ef4be235e88e83a90c554487286e8721d
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-11-28 19:57:57 -0500

    added all kent

--- app/controllers/api/kents_controller.rb
+++ app/controllers/api/kents_controller.rb
@@ -25,13 +25,13 @@ class Api::KentsController < ApplicationController
     
   def update
       @kent = Kent.find_by(id: params[:id])
-      @kent.superman: params[:superman],
-      @kent.lois: params[:lois],
-      @kent.luthor: params[:@luthor],
-      @kent.luthor: params[:luthor],
-      @kent.lois: params[:lois],
-      @kent.director: params[:director],
-      @kent.description: params[:description],
+      @kent.superman: = params[:superman],
+      @kent.lois: = params[:lois],
+      @kent.luthor: = params[:@luthor],
+      @kent.luthor: = params[:luthor],
+      @kent.lois: = params[:lois],
+      @kent.director: = params[:director],
+      @kent.description: = params[:description],
       @kent.save
       render "show.json.jb"
   end
